7bf315e07fbc1d28fa1b0799a91379f028739577
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2023-09-03 21:22:01 -0300

    first commit

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {Button} from "@/components/ui/button";
+import {Menu} from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+import {useEffect, useState} from "react";
+
+const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState (false);
+
+        useEffect (() => {
+            setIsMounted (true);
+
+        }, [] );
+
+        if (!isMounted) {
+            return null;
+        }
+
+
+
+
+
+        return (
+        <Sheet>
+            <SheetTrigger>
+             <button variant="ghost" size="icon" className="md:hidden">
+                <Menu/>
+            </button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar/>
+            </SheetContent>
+        </Sheet>
+       
+           
+       
+    );
+}
+
+export default MobileSidebar;
---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+import { Menu } from "lucide-react";
+
+import MobileSidebar from "@/components/mobile-sidebar";
+
+
+const Navbar = () =>{
+    return (
+        <div className="flex items-center p-4">
+        <MobileSidebar/>
+        </div>
+    );
+    
+}
+export default Navbar;
